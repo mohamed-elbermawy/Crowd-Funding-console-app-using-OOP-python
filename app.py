604a61df62567d23fd5e83958d3bf62478d00320
mohamed-elbermawy <mohamed77bermawy@gmail>
2021-05-02 12:09:15 +0200

    add project feature

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from controller.super_admin_controller import SuperAdminController
+from controller.normal_user_controller import NormalUserController
 from controller.admin_controller import AdminController
 from helpers.helpers import Helpers
+from models.project import Project
 from models.admin import Admin
 from models.user import User
 
 
 super_admin_controller = SuperAdminController(Helpers)
+normal_user_controller = NormalUserController(Helpers)
 admin_controller = AdminController(Helpers)
 helper = Helpers()
 
 # admin = Admin("taha","ashraf","a@gmail.com","123","123","000",1,0,1)
-user = User("mohamed","ashraf","a@gmail.com","123","123","000")
+# user = User("mohamed","ashraf","a@gmail.com","123","123","000")
+project = Project("pro","pro",20000,"2020-2-30","2022-5-25","a@gmail.com")
 
 # super_admin_controller.add_admin(admin)
 # super_admin_controller.dactivate_admin("aa@gmail.com")
@@ -21,5 +25,7 @@ user = User("mohamed","ashraf","a@gmail.com","123","123","000")
 
 # admin_controller.add_user(user)
 # admin_controller.delete_user("a@gmail.com")
+
+normal_user_controller.add_project(project)
 
 # print(helper.isemail_exsist(user.email,"db/user.json"))
--- controller/normal_user_controller.py
+++ controller/normal_user_controller.py
@@ -1,3 +1,20 @@
 class NormalUserController:
     def __init__(self,Helpers):
         self.helper = Helpers()
+
+    def add_project(self,project):
+        new_project = {}
+
+        data = self.helper.loadData("db/project.json")
+
+        new_project['title']= project.title   
+        new_project['details']= project.details
+        new_project['total_target']= project.total_target
+        new_project['start_time']= project.start_time
+        new_project['end_time']= project.end_time
+        new_project['auther']= project.auther
+
+        data.append(new_project)
+
+        self.helper.saveData("db/project.json",data)
+        print("Added")
